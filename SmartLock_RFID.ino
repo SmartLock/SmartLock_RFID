9c8b752a9dae60461d538df04a2de22ce532856b
Fernando C. de Carvalho Neto <fcarvalho83@icloud.com>
2015-06-22 22:18:25 -0300

    alterado tempo de abertura para 60

--- SmartLock_RFID.ino
+++ SmartLock_RFID.ino
@@ -30,7 +30,7 @@ byte masterCard[4];   // Stores master card's ID read from EEPROM
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create MFRC522 instance.
 
-int tempoAbertura = 20;
+int tempoAbertura = 60;
 int tempoDelayCiclo = 2500;
 int tempoStatusLed = 250;
 
